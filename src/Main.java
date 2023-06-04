832e290b45424369392307230a729751410f5000
Evgeny Shchegortsov <ESHCHegortsov@inno.tech>
2023-06-05 01:17:21 +0300

    The first initial commit of program regarding data types and basic operations: sum, minus, prodct, quotient

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in) .nextLine();
-        System.out.println("Длина текста: " + text.length());
+        System.out.println("Введите число и нажмите <Enter> ");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите следующее число и нажмите <Enter> ");
+        int number2 = new Scanner(System.in).nextInt();
+        int firstNumber = 5;
+        int secondNumber = 10;
+        double quotient = (double) firstNumber / secondNumber;
+        System.out.println("Сумма двух чисел = "+ (firstNumber+secondNumber));
+        System.out.println("Разность двух чисел = "+ (firstNumber-secondNumber));
+        System.out.println("Умножение двух чисел = "+ (firstNumber*secondNumber));
+        System.out.println("Частное = "+ (quotient));
     }
 }
