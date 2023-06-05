cd274739004b4ee25b5a8f8ca7b1f920e5fb2d5e
Evgeny Shchegortsov <ESHCHegortsov@inno.tech>
2023-06-06 02:03:46 +0300

    The second commit includes operations with numbers those are specified via console

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,10 @@ public class Main {
         int number1 = new Scanner(System.in).nextInt();
         System.out.println("Введите следующее число и нажмите <Enter> ");
         int number2 = new Scanner(System.in).nextInt();
-        int firstNumber = 5;
-        int secondNumber = 10;
-        double quotient = (double) firstNumber / secondNumber;
-        System.out.println("Сумма двух чисел = "+ (firstNumber+secondNumber));
-        System.out.println("Разность двух чисел = "+ (firstNumber-secondNumber));
-        System.out.println("Умножение двух чисел = "+ (firstNumber*secondNumber));
+        double quotient = (double) number1 / number2;
+        System.out.println("Сумма двух чисел = "+ (number1+number2));
+        System.out.println("Разность двух чисел = "+ (number1-number2));
+        System.out.println("Умножение двух чисел = "+ (number1*number2));
         System.out.println("Частное = "+ (quotient));
     }
 }
